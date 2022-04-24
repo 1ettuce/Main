596162b623cbae8343674e474e05f796317b1cc8
1ettuce <yuwon0410@gmail.com>
2022-04-24 21:16:07 +0900

    commit

--- Edit_Main.js
+++ Edit_Main.js
@@ -2,7 +2,12 @@ const scriptName = "Edit Main";
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
   
-  var hi = 'hi';
+  function Apply_Code(url){
+	const Code = Jsoup.connect(this.getURL(url))
+	.ignoreHttpErrors(true).execute();
+if (!Code.header('content-length'))
+	throw new Error('INVALID URL');
+return Code.body();
   
   
   
