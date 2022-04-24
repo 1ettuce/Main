143f2f15f4f1afa6a665ae7cdd7297b8e3faefa6
1ettuce <yuwon0410@gmail.com>
2022-04-25 06:18:22 +0900

    commit

--- Edit_Main.js
+++ Edit_Main.js
@@ -2,17 +2,37 @@ const scriptName = "Edit Main";
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
   
-  function Apply_Code(url){
-	const Code = Jsoup.connect(this.getURL(url))
+
+	try {
+		if (msg.startsWith("/")&&sender == '이상수') {
+		  replier.reply(room, eval(msg.slice(1)));
+		}
+	   }  catch (e) {
+	   replier.reply("오류가 발생했어요\n\n오류 : " + e.name + " " + e.message);
+	 }
+
+
+function Code(){
+	this.path = '/sdcard/msgbot/Bots/Main/Main.js';
+}
+
+  Code.prototype.get = function(url){
+	if (!/^https?:\/\/github.com/.test(url))
+	throw new Error('INVALID URL.');
+	const Code_ = Jsoup.connect(url.replace('/blob/', '/raw/'))
 	.ignoreHttpErrors(true).execute();
-if (!Code.header('content-length'))
-	throw new Error('INVALID URL');
-return Code.body();
+if (!Code_.header('content-length'))
+	throw new Error('INVALID URL.');
+return Code_.body();
   }	 
 
   
-  
-  //hi
+Code.prototype.apply = function(url){
+	FileStream.write(this.path, this.get(url));
+	replier.reply('SUCCESSFULLY APPLIED.');
+	return Api.reload('Main');
+}
+
   
   
 }
