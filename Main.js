af0d965f6759019e5819e9faf13b2490b114af97
1ettuce <yuwon0410@gmail.com>
2022-04-24 19:43:09 +0900

    ab

--- Main.js
+++ Main.js
@@ -2,5 +2,5 @@ const script_name = "Main.js"
 const script_name1 = "Gipo.js"
 const script_name2 = "Gifive.js"
 
-replier.reply('hi');
+replier.reply('hi!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
 replier.reply('no!!!!!!!!!!');
