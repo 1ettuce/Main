3d1c7a87da56c6ba4da436f91b8bde69ddb49245
1ettuce <yuwon0410@gmail.com>
2022-04-26 05:43:16 +0900

    g

--- Edit_Main.js
+++ Edit_Main.js
@@ -9,27 +9,26 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
     if (msg.startsWith("code.") && sender == '이상수') {
       replier.reply(room, eval(msg.slice(5).trim()));
     }
-  }  catch (e) {
-  replier.reply("an " + e.name + " occurred.\n: " + e.message);
-}
+  } catch (e) {
+    replier.reply("an " + e.name + " occurred.\n: " + e.message);
+  }
 
-Date.prototype.Time = function() {
-  return this.getFullYear() + '-' + (this.getMonth() + 1) + '-' + this.getDate() + '  ' + this.getHours() + ':' + this.getMinutes() + ':' + this.getSeconds();
-}
+  Date.prototype.Time = function () {
+    return this.getFullYear() + '-' + (this.getMonth() + 1) + '-' + this.getDate() + '  ' + this.getHours() + ':' + this.getMinutes() + ':' + this.getSeconds();
+  }
 
   function get(url) {
-    if (!/^https?:\/\/github.com/.test(url)) 
+    if (!/^https?:\/\/github.com/.test(url))
       throw new Error('INVALID URL.');
     const Code_ = Jsoup.connect(url.replace('/blob/', '/raw/')).ignoreHttpErrors(true).execute();
-    if (!Code_.header('content-length')) 
+    if (!Code_.header('content-length'))
       throw new Error('INVALID URL.');
     return Code_.body();
-  }
-  ;
+  };
+
   function apply(url) {
-	FileStream.write('/sdcard/msgbot/Bots/Main/Backup/'+new Date().Time()+'.js', FileStream.read('/sdcard/msgbot/Bots/Main/Main.js'));
+    FileStream.write('/sdcard/msgbot/Bots/Main/Backup/' + new Date().Time() + '.js', FileStream.read('/sdcard/msgbot/Bots/Main/Main.js'));
     FileStream.write('/sdcard/msgbot/Bots/Main/Main.js', get(url));
-    return Api.reload('Main') , 'SUCCESSFULLY APPLIED.';
-  }
-  ;
+    return Api.reload('Main'), 'SUCCESSFULLY APPLIED.';
+  };
 }
