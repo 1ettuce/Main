42e3de42a2350ab0b02f29fc2160249b7be899eb
1ettuce <yuwon0410@gmail.com>
2022-05-08 22:58:18 +0900

    1.18.4

--- Main.js
+++ Main.js
@@ -61,11 +61,11 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
   if (msg.startsWith('d.day')) {
     try {
       var d_day_list = {
-        '수능': '2022/11/17'
+        '2023 대학수학능력시험': '2022/11/17'
       }
       if (!Object.keys(d_day_list).includes(msg.slice(5).trim())) return;
       var d_day = Math.ceil((new Date(d_day_list[msg.slice(5).trim()]) - new Date()) / 86400000);
-      replier.reply(Noti_Room.includes(room) ? Send_Room : room, '[' + msg.slice(5).trim() + '] ' + (d_day > 0 ? 'D-' + d_day : d_day == 0 ? 'D-Day' : 'D+' + Math.abs(d_day)));
+      replier.reply(Noti_Room.includes(room) ? Send_Room : room, '[' + msg.slice(5).trim() + ']\n━━━━━━━━━━\n' + (d_day > 0 ? 'D-' + d_day : d_day == 0 ? 'D-Day' : 'D+' + Math.abs(d_day)) +'   ('+ (((365-d_day)/365)*100).toFixed(2) + '% 경과)');
     } catch (e) {
       replier.reply('Error: ' + e.message);
     }
