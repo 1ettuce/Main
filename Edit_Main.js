60d1554d32e1fab4554e6f894ca63d379871ca58
1ettuce <yuwon0410@gmail.com>
2022-05-16 22:15:33 +0900

    1.0

--- Edit_Main.js
+++ Edit_Main.js
@@ -1,10 +1,6 @@
 const scriptName = "Edit Main";
 Jsoup = org.jsoup.Jsoup;
 
-Date.prototype.Time = function () {
-  return this.getFullYear() + '-' + (this.getMonth() + 1) + '-' + this.getDate() + '  ' + this.getHours() + ':' + this.getMinutes() + ':' + this.getSeconds();
-}
-
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
   msg = msg.trim();
   sender = sender.trim().replace("~#~", "");
@@ -17,18 +13,25 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
     replier.reply("an " + e.name + " occurred.\n: " + e.message);
   }
 
+  Date.prototype.Time = function () {
+    return this.getFullYear() + '-' + (this.getMonth() + 1) + '-' + this.getDate() + '  ' + this.getHours() + ':' + this.getMinutes() + ':' + this.getSeconds();
+  }
+
   function get(url) {
+    url = url == null ? 'https://github.com/1ettuce/Main/blob/main/Main.js' : url
     if (!/^https?:\/\/github.com/.test(url))
       throw new Error('INVALID URL.');
     const Code_ = Jsoup.connect(url.replace('/blob/', '/raw/')).ignoreHttpErrors(true).execute();
+    const Version_ = Jsoup.connect('https://github.com/1ettuce/Main/commits/main/Main.js').get().select('#repo-content-pjax-container > div > div.js-navigation-container.js-active-navigation-container.mt-3 > div.TimelineItem.TimelineItem--condensed.pt-0.pb-2 > div.TimelineItem-body > ol > li:nth-child(1) > div.flex-auto.min-width-0.js-details-container.Details > p > a').text();
     if (!Code_.header('content-length'))
       throw new Error('INVALID URL.');
-    return Code_.body();
+    return [Version_, Code_.body()];
   };
 
   function apply(url) {
     FileStream.write('/sdcard/msgbot/Bots/Main/Backup/' + new Date().Time() + '.js', FileStream.read('/sdcard/msgbot/Bots/Main/Main.js'));
-    FileStream.write('/sdcard/msgbot/Bots/Main/Main.js', get(url));
+    FileStream.write('/sdcard/msgbot/Bots/Main/Main.js', get(url)[1]);
+    FileStream.write('/sdcard/msgbot/Bots/Main/Version.txt', get(url)[0]);
     return Api.reload('Main'), 'SUCCESSFULLY APPLIED.';
   };
 }
