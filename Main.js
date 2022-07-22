433c72b0b6aad6b0c19411a409f4134ac9df4c3c
1ettuce <yuwon0410@gmail.com>
2022-07-22 18:06:21 +0900

    1.26.15a

--- Main.js
+++ Main.js
@@ -242,10 +242,11 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
         break;
       case ['reload', 'update'].includes(msg.toLowerCase().slice(5)):
         var Changes_Count = []
+        var Changes_Check = []
         for (fi = 0; fi < User_Array.length; fi++) {
           if (User_Format[fi].length > User_Latest_Format[fi].length) {
             for (si = 0; si < Object.keys(User_Array[fi]).length; si++) {
-              Array.prototype.push.apply(User_Array[fi][Object.keys(User_Array[fi])[si]], User_Format[fi].splice(User_Latest_Format[fi].length - User_Format[fi].length));
+              Changes_Check.push(User_Array[fi][Object.keys(User_Array[fi])[si]], User_Format[fi].splice(User_Latest_Format[fi].length - User_Format[fi].length));
               Changes_Count.push(User_Format[fi].length - User_Latest_Format[fi].length);
             }
             FileStream.write(user + ['user', 'date', 'stats'][fi] + '.json', JSON.stringify(User_Array[fi]));
@@ -253,7 +254,7 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
         }
         if (Changes_Count.length > 0) {
           FileStream.write(general + 'User_Latest_Format.json', JSON.stringify(User_Format));
-          replier.reply('Saved ' + Changes_Count.join() + ' changes.');
+          replier.reply('Saved ' + Changes_Count.join() + ' changes.\n'+Changes_Check.join());
         } else {
           replier.reply('No changes.');
         }
